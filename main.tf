835d0d618dfee92621da5be2a6b7919ecc789132
Krishnaveni0321 <krishnaveni2102@gmail.com>
2024-12-02 16:54:38 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
 
+resource "local_file" "names" {
+  filename = var.filename
+  content  = var.content
+}
+resource "random_pet" "petname"{
+    prefix = "MR"
+    separator = "."
+    length = "1"
+}
